2c7248c21b509da1daa111d6bcf9fc60f9d34f9c
Koudous13 <153217209+Koudous13@users.noreply.github.com>
2024-10-15 18:42:45 +0100

    Update app.py
    Signed-off-by: Koudous13 <153217209+Koudous13@users.noreply.github.com>

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def download_and_load_model():
    try:
         if not os.path.exists(output):
             gdown.download(url, output, quiet=False)
-        model = load_model(output, custom_objects=None, compile=True, safe_mode=True)
+        model = load_model(output, custom_objects=None, compile=True)
         return model
    except Exception as e:
         st.error("Erreur lors du chargement du modèle: " + str(e))
